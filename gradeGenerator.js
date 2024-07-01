4ec30d8e0f41562c2e88a3955f7265b4549999f9
shantal rohbi <shantal.rohbi@student.moringaschool.com>
2024-07-01 15:12:39 +0300

    i edited the gradeGenerator

--- gradeGenerator.js
+++ gradeGenerator.js
@@ -1,36 +1,22 @@
 //creates an input for user
-const input = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
 
-function awardGrade(marks) {
-    if (marks > 100 || marks < 0) {
-        return 'invalid mark'
-    }
-    if (marks > 79) {
-        return 'A'
-    } else if (marks >= 60 && marks <= 79) {
-        return 'B'
-    } else if (marks >= 49 && marks <= 59) {
-        return 'C'
-    } else if (marks >= 40 && marks <= 49) {
-        return 'D'
-    } else { marks <40
-        return 'E'
-    }
-}
-//input question
-input.question('enter student marks: ', function (score) {
-    const mark = Number(score)
-    if (isNaN(mark)) {
-        console.log('please enter valid marks')
-    } else {
-        const grade = awardGrade(mark)
-        console.log(`the grade is ${grade}`)
-    }
 
-    input.close()
-
-}) 
- 
+function awardGrade() {
+    const input = prompt("Weka marks: ")
+        if (input > 100 || input < 0) {
+            return 'invalid mark'
+        }
+        if (input > 79) {
+            return 'A'
+        } else if (input >= 60 && input <= 79) {
+            return 'B'
+        } else if (input >= 49 && input <= 59) {
+            return 'C'
+        } else if (input >= 40 && input <= 49) {
+            return 'D'
+        } else { input <40
+            return 'E'
+        }
+    }
+    //input question
+    console.log(awardGrade())
